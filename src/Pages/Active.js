fb98119a610750f7b96f98b147ce60ba78972ecf
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-02-15 18:25:59 +0100

    adding active page content and css

---
+++ src/Pages/Active.js
@@ -0,0 +1,40 @@
+import { useSelector } from "react-redux";
+import { selectNfts } from "../features/nftsSlice";
+import NFT from "../components/NFT/NFT";
+import './Active.css';
+import SideBar from "../components/SideBar/SideBar";
+
+const Active =() => {
+    const nfts = useSelector(selectNfts);
+    const activeNfts = nfts.filter(nft => nft.isExpired === false) 
+    if(activeNfts.length === 0 ){
+        return <div>No Active NFTs</div>
+    }
+    return (
+        <div id="activePage">
+            <div id='main-content'>
+                <div id="trending-nfts">
+                    <ul>
+                        {
+                            activeNfts.map(nft => {
+                                if (nft.isExpired === true) return null;
+                                else {
+                                    return (
+                                        (
+                                            <li key={nft.id}>
+                                                <NFT nft={nft} />
+                                            </li>
+                                        )
+                                    )
+                                }
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+            <SideBar />
+        </div>
+        
+    )
+}
+export default Active;
--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import NavMenu from '../components/NavMenu/NavMenu';
 import Header from '../components/Header/Header';
 import './App.css';
 import Home from '../components/Home/Home';
+import Active from '../Pages/Active';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/active" element={<Active />} />
         </Routes>
       </main>
     </div>
