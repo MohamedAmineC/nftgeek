b3db36e501bcd2593b7c8bdb6ca1e7b65dd2e4df
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-02-15 12:27:06 +0100

    seperating concerns and creating an NFT component

--- src/components/Home/Trends.js
+++ src/components/Home/Trends.js
@@ -1,11 +1,8 @@
-import { useDispatch, useSelector } from "react-redux";
-import { toggleLikeNft, selectNfts } from "../../features/nftsSlice";
-import { addFavorites, removeFavorites } from "../../features/ProfileSlice";
-import Countdown from "./Countdown";
+import { useSelector } from "react-redux";
+import { selectNfts } from "../../features/nftsSlice";
+import NFT from "../NFT/NFT";
 
-const Trends = (props) => {
-    const {category} = props;
-    const dispatch = useDispatch();
+const Trends = ({category}) => {
     const nfts = useSelector(selectNfts);
     const filteredNfts = nfts.filter(nft => nft.category === category);
     
@@ -20,74 +17,7 @@ const Trends = (props) => {
                             return (
                                 (
                                     <li key={nft.id}>
-                                        <div className="nft-image" style={{
-                                            backgroundImage: `url(${nft.img})`
-                                        }}>
-                                            
-                                            {nft.liked ? (
-                                                <div className="liked" onClick={() => {
-                                                    dispatch(toggleLikeNft(nft));
-                                                    dispatch(removeFavorites(nft))
-            
-                                                }}>
-                                                    <i class="bi bi-heart-fill"></i>
-                                                </div>
-                                            ) : (
-                                                <div className="like" onClick={(e) => {
-                                                    const currentDate = new Date();
-                                                    const options = { weekday: 'short', day: 'numeric', month: 'short' };
-                                                    const formattedDate = currentDate.toLocaleDateString('en-US', options);
-                                                    dispatch(toggleLikeNft(nft));
-                                                    dispatch(addFavorites({
-                                                       ...nft,
-                                                        liked: formattedDate,
-                                                    }))
-                                                }}>
-                                                    <i class="bi bi-heart-fill"></i>
-                                                </div>
-                                            ) }
-                                            
-                                        </div>
-                                        <div className="nft-description">
-                                            <div>
-                                                <p>{nft.name}</p>
-                                                <p>- {nft.author}</p>
-                                            </div>
-                                            <div className="nft-time-remaining">
-                                                <Countdown expires={nft.expires} id={nft.id} />
-                                            </div>
-                                        </div>
-                                        <ul className="bids-list">
-                                            {nft.bids.map(bid => (
-                                                <li key={bid.id}>
-                                                    <div className="bid-bidder">
-                                                        <div className="bidder-image" style={{
-                                                            backgroundImage: `url(${bid.bidderImg})`
-                                                        }}>
-                                                            {bid.verified && <i class="bi bi-patch-check-fill"></i>}
-                                                        </div>
-                                                        <div className="bidder-info">
-                                                            <p>Creator</p>
-                                                            <p>{bid.bidder}</p>
-                                                        </div>
-                                                    </div>
-                                                    <div className="bid-price">
-                                                        <div className="bid-image" style={{
-                                                            backgroundImage: `url(${bid.currency})`
-                                                        }}>
-            
-                                                        </div>
-                                                        <div className="bid-pricing">
-                                                            <p>Current Bid</p>
-                                                            <p>{bid.price} {bid.currencyName}</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                        <div>
-                                            <p>Place a Bid</p>
-                                        </div>
+                                        <NFT nft={nft} />
                                     </li>
                                 )
                             )
---
+++ src/components/NFT/NFT.js
@@ -0,0 +1,81 @@
+import { useDispatch } from "react-redux";
+import { toggleLikeNft } from "../../features/nftsSlice";
+import { addFavorites, removeFavorites } from "../../features/ProfileSlice";
+import Countdown from "../Home/Countdown";
+
+const NFT = ({nft}) => {
+    const dispatch = useDispatch();
+    return (
+        <>
+        <div className="nft-image" style={{
+            backgroundImage: `url(${nft.img})`
+        }}>
+            
+            {nft.liked ? (
+                <div className="liked" onClick={() => {
+                    dispatch(toggleLikeNft(nft));
+                    dispatch(removeFavorites(nft))
+
+                }}>
+                    <i class="bi bi-heart-fill"></i>
+                </div>
+            ) : (
+                <div className="like" onClick={(e) => {
+                    const currentDate = new Date();
+                    const options = { weekday: 'short', day: 'numeric', month: 'short' };
+                    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+                    dispatch(toggleLikeNft(nft));
+                    dispatch(addFavorites({
+                       ...nft,
+                        liked: formattedDate,
+                    }))
+                }}>
+                    <i class="bi bi-heart-fill"></i>
+                </div>
+            ) }
+            
+        </div>
+        <div className="nft-description">
+            <div>
+                <p>{nft.name}</p>
+                <p>- {nft.author}</p>
+            </div>
+            <div className="nft-time-remaining">
+                <Countdown expires={nft.expires} id={nft.id} />
+            </div>
+        </div>
+        <ul className="bids-list">
+            {nft.bids.map(bid => (
+                <li key={bid.id}>
+                    <div className="bid-bidder">
+                        <div className="bidder-image" style={{
+                            backgroundImage: `url(${bid.bidderImg})`
+                        }}>
+                            {bid.verified && <i class="bi bi-patch-check-fill"></i>}
+                        </div>
+                        <div className="bidder-info">
+                            <p>Creator</p>
+                            <p>{bid.bidder}</p>
+                        </div>
+                    </div>
+                    <div className="bid-price">
+                        <div className="bid-image" style={{
+                            backgroundImage: `url(${bid.currency})`
+                        }}>
+
+                        </div>
+                        <div className="bid-pricing">
+                            <p>Current Bid</p>
+                            <p>{bid.price} {bid.currencyName}</p>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+        <div>
+            <p>Place a Bid</p>
+        </div>
+        </>
+    )
+}
+export default NFT;
